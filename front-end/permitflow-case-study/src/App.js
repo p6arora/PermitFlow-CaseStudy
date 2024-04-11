9b584132a03669bb3aac2b763e580b1d3c0241cf
Paarth Arora <paarth.arora21@gmail.com>
2024-04-11 13:41:03 -0400

    Integrated the dropdown into App.js

--- front-end/permitflow-case-study/src/App.js
+++ front-end/permitflow-case-study/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Dropdown from './components/Dropdown';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className='app'>
       <Header/>
+      <h2>What Residential Work Are You Doing?</h2>
+      <Dropdown/>
      
     </div>
   );
