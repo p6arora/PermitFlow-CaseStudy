fc8fa636f9e10c36520e751b7f2fa1299bbe88c6
Paarth Arora <paarth.arora21@gmail.com>
2024-04-11 14:25:28 -0400

    Added options for exterior component

--- front-end/permitflow-case-study/src/components/Exterior.js
+++ front-end/permitflow-case-study/src/components/Exterior.js
@@ -1,7 +1,73 @@
+import { useState } from 'react';
+
 const Exterior = () => {
-  return (
-    <div>Exterior</div>
-  )
+    // State to keep track of the checked state of each checkbox
+    const [checkboxes, setCheckboxes] = useState({
+        garageDoorReplacement: false,
+        exteriorDoors: false,
+        fencing: false,
+        other: false
+    });
+
+    // Handler function to toggle the checked state of a checkbox
+    const handleCheckboxChange = (event) => {
+        const { name, checked } = event.target;
+        setCheckboxes({
+            ...checkboxes,
+            [name]: checked
+        });
+    };
+    return (
+        <div>
+            <h3>What exterior work are you doing?</h3>
+            {/* Checkbox input elements */}
+            <label>
+                <input
+                    type="checkbox"
+                    name="garageDoorReplacement"
+                    checked={checkboxes.garageDoorReplacement}
+                    onChange={handleCheckboxChange}
+                />
+                Garage Door Replacement
+            </label>
+            <label>
+                <input
+                    type="checkbox"
+                    name="exteriorDoors"
+                    checked={checkboxes.exteriorDoors}
+                    onChange={handleCheckboxChange}
+                />
+                Exterior Doors
+            </label>
+            <label>
+                <input
+                    type="checkbox"
+                    name="fencing"
+                    checked={checkboxes.fencing}
+                    onChange={handleCheckboxChange}
+                />
+                Fencing
+            </label>
+            <label>
+                <input
+                    type="checkbox"
+                    name="other"
+                    checked={checkboxes.other}
+                    onChange={handleCheckboxChange}
+                />
+                Other
+            </label>
+            {/* Display the checked state of each checkbox */}
+            <div>
+                <p>Garage Door Replacement is {checkboxes.garageDoorReplacement ? 'checked' : 'unchecked'}</p>
+                <p>Exteior Doors is {checkboxes.exteriorDoors ? 'checked' : 'unchecked'}</p>
+                <p>Fencing is {checkboxes.fencing ? 'checked' : 'unchecked'}</p>
+                <p>Other is {checkboxes.other ? 'checked' : 'unchecked'}</p>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Exterior
