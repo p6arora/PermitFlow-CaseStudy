f2aee18b8e6027bc107950bc04f9bd20375b55a0
Paarth Arora <paarth.arora21@gmail.com>
2024-04-11 14:14:43 -0400

    Added code to create checkboxes for user to select what options they'd like for their modeling

--- front-end/permitflow-case-study/src/components/Interior.js
+++ front-end/permitflow-case-study/src/components/Interior.js
@@ -1,7 +1,71 @@
+import { useState } from 'react';
+
 const Interior = () => {
-  return (
-    <div>Interior</div>
-  )
+    // State to keep track of the checked state of each checkbox
+    const [checkboxes, setCheckboxes] = useState({
+        bathroomRemodel: false,
+        newBathroom: false,
+        newLaundryRoom: false,
+        other: false
+    });
+
+    // Handler function to toggle the checked state of a checkbox
+    const handleCheckboxChange = (event) => {
+        const { name, checked } = event.target;
+        setCheckboxes({
+            ...checkboxes,
+            [name]: checked
+        });
+    };
+    return (
+        <div>
+            <h3>What interior work are you doing?</h3>
+            {/* Checkbox input elements */}
+            <label>
+                <input
+                    type="checkbox"
+                    name="bathroomRemodel"
+                    checked={checkboxes.bathroomRemodel}
+                    onChange={handleCheckboxChange}
+                />
+                Bathroom Remodel
+            </label>
+            <label>
+                <input
+                    type="checkbox"
+                    name="newBathroom"
+                    checked={checkboxes.newBathroom}
+                    onChange={handleCheckboxChange}
+                />
+                New Bathroom
+            </label>
+            <label>
+                <input
+                    type="checkbox"
+                    name="newLaundryRoom"
+                    checked={checkboxes.newLaundryRoom}
+                    onChange={handleCheckboxChange}
+                />
+                New Laundry Room
+            </label>
+            <label>
+                <input
+                    type="checkbox"
+                    name="other"
+                    checked={checkboxes.other}
+                    onChange={handleCheckboxChange}
+                />
+                Other
+            </label>
+            {/* Display the checked state of each checkbox */}
+            <div>
+                <p>Bathroom Remodel is {checkboxes.bathroomRemodel ? 'checked' : 'unchecked'}</p>
+                <p>New Bathroom is {checkboxes.newBathroom ? 'checked' : 'unchecked'}</p>
+                <p>New Laundry Room is {checkboxes.newLaundryRoom ? 'checked' : 'unchecked'}</p>
+                <p>Other is {checkboxes.other ? 'checked' : 'unchecked'}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Interior
