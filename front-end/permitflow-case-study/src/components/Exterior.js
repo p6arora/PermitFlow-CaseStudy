cd3b9ea68770326f071448210ccead9e50af2106
Paarth Arora <paarth.arora21@gmail.com>
2024-04-12 19:46:41 -0400

    Added a new div for all checkboxes in the interior and exterior checkboxes

--- front-end/permitflow-case-study/src/components/Exterior.js
+++ front-end/permitflow-case-study/src/components/Exterior.js
@@ -9,44 +9,46 @@ const Exterior = ({ checkboxes, setCheckBoxes }) => {
     };
     return (
         <div>
-            <h3>What exterior work are you doing?</h3>
-            {/* Checkbox input elements */}
-            <label>
-                <input
-                    type="checkbox"
-                    name="garageDoorReplacement"
-                    checked={checkboxes.garageDoorReplacement}
-                    onChange={handleCheckboxChange}
-                />
-                Garage Door Replacement
-            </label>
-            <label>
-                <input
-                    type="checkbox"
-                    name="exteriorDoors"
-                    checked={checkboxes.exteriorDoors}
-                    onChange={handleCheckboxChange}
-                />
-                Exterior Doors
-            </label>
-            <label>
-                <input
-                    type="checkbox"
-                    name="fencing"
-                    checked={checkboxes.fencing}
-                    onChange={handleCheckboxChange}
-                />
-                Fencing
-            </label>
-            <label>
-                <input
-                    type="checkbox"
-                    name="other"
-                    checked={checkboxes.other}
-                    onChange={handleCheckboxChange}
-                />
-                Other
-            </label>
+            <h3>What Exterior Work Are You Doing?</h3>
+            <div className="checkbox-container">
+                {/* Checkbox input elements */}
+                <label>
+                    <input
+                        type="checkbox"
+                        name="garageDoorReplacement"
+                        checked={checkboxes.garageDoorReplacement}
+                        onChange={handleCheckboxChange}
+                    />
+                    Garage Door Replacement
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="exteriorDoors"
+                        checked={checkboxes.exteriorDoors}
+                        onChange={handleCheckboxChange}
+                    />
+                    Exterior Doors
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="fencing"
+                        checked={checkboxes.fencing}
+                        onChange={handleCheckboxChange}
+                    />
+                    Fencing
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="other"
+                        checked={checkboxes.other}
+                        onChange={handleCheckboxChange}
+                    />
+                    Other
+                </label>
+            </div>
         </div>
     )
 }
--- front-end/permitflow-case-study/src/components/Interior.js
+++ front-end/permitflow-case-study/src/components/Interior.js
@@ -1,4 +1,4 @@
-const Interior = ({checkboxes, setCheckBoxes}) => {
+const Interior = ({ checkboxes, setCheckBoxes }) => {
     // Handler function to toggle the checked state of a checkbox
     const handleCheckboxChange = (event) => {
         const { name, checked } = event.target;
@@ -9,44 +9,46 @@ const Interior = ({checkboxes, setCheckBoxes}) => {
     };
     return (
         <div>
-            <h3>What interior work are you doing?</h3>
-            {/* Checkbox input elements */}
-            <label>
-                <input
-                    type="checkbox"
-                    name="bathroomRemodel"
-                    checked={checkboxes.bathroomRemodel}
-                    onChange={handleCheckboxChange}
-                />
-                Bathroom Remodel
-            </label>
-            <label>
-                <input
-                    type="checkbox"
-                    name="newBathroom"
-                    checked={checkboxes.newBathroom}
-                    onChange={handleCheckboxChange}
-                />
-                New Bathroom
-            </label>
-            <label>
-                <input
-                    type="checkbox"
-                    name="newLaundryRoom"
-                    checked={checkboxes.newLaundryRoom}
-                    onChange={handleCheckboxChange}
-                />
-                New Laundry Room
-            </label>
-            <label>
-                <input
-                    type="checkbox"
-                    name="other"
-                    checked={checkboxes.other}
-                    onChange={handleCheckboxChange}
-                />
-                Other
-            </label>
+            <h3>What Interior Work Are You Doing?</h3>
+            <div className="checkbox-container">
+                {/* Checkbox input elements */}
+                <label>
+                    <input
+                        type="checkbox"
+                        name="bathroomRemodel"
+                        checked={checkboxes.bathroomRemodel}
+                        onChange={handleCheckboxChange}
+                    />
+                    Bathroom Remodel
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="newBathroom"
+                        checked={checkboxes.newBathroom}
+                        onChange={handleCheckboxChange}
+                    />
+                    New Bathroom
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="newLaundryRoom"
+                        checked={checkboxes.newLaundryRoom}
+                        onChange={handleCheckboxChange}
+                    />
+                    New Laundry Room
+                </label>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="other"
+                        checked={checkboxes.other}
+                        onChange={handleCheckboxChange}
+                    />
+                    Other
+                </label>
+            </div>
         </div>
     )
 }
